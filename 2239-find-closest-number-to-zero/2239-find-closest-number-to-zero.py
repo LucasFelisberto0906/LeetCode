df452b429ace3e8551061bbe73f547bacedc6631
Lucas Felisberto <96011281+LucasFelisberto0906@users.noreply.github.com>
2025-06-03 20:46:44 +0100

    Time: 35 ms (5.11%), Space: 18.1 MB (28.22%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.py
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.py
@@ -2,8 +2,7 @@ class Solution:
     def findClosestNumber(self, nums: List[int]) -> int:
         distancias = {}
         for num in nums:
-            distancia = abs(num)
-            distancias[num] = distancia
+            distancias[num] = abs(num)
         
         ordenado = dict(sorted(distancias.items(), key=lambda item: (item[1], -item[0])))
         return list(ordenado.keys())[0]
